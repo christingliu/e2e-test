37bc602ace2bfd7df696962e7ed508d5222dc1cf
liu <2945732712@qq.com>
2018-02-09 08:55:56 +0800

    add nightwatch.conf.js

---
+++ nightwatch.conf.js
@@ -0,0 +1,43 @@
+{
+  "src_folders" : ["tests"],
+  "output_folder" : "reports",
+  "custom_commands_path" : "",
+  "custom_assertions_path" : "",
+  "page_objects_path" : "",
+  "globals_path" : "",
+   "test_workers":true,
+
+  "selenium" : {
+    "start_process" :true,
+    "server_path" : "bin/selenium-server-standalone-3.8.1.jar",
+    "log_path" : "",
+    "port" : 4444,
+    "cli_args" : {
+      "webdriver.chrome.driver" : "bin/chromedriver.exe",
+      "webdriver.gecko.driver" : "bin/geckodriver.exe",
+      "webdriver.edge.driver" : ""
+    }
+  },
+ 
+  "test_settings" : {
+    "default" : {
+      "launch_url" : "http://localhost",
+      "selenium_port"  : 4444,
+      "selenium_host"  : "localhost",
+      "silent": true,
+      "screenshots" : {
+        "enabled" : true,
+        "path" : ""
+      },
+      "desiredCapabilities": {
+         "browserName":"firefox",
+         "marionette":true
+      }
+    },
+    "chrome":{
+      "desiredCapabilities": {
+         "browserName": "chrome"
+       }
+    }
+  }
+}
